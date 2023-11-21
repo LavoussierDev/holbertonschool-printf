a5364c51b3372c6693c307bb1dc02ec7105c5291
LavoussierDev <Onilr31@gmail.com>
2023-11-20 23:31:38 -0400

    Adding files & Directories

---
+++ printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf - Prints characters, converts format specifier,
+ * numbers, special characters, essentially it's a clone
+ * to the printf function in the standard library
+ *
+ * @format:
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+
+
+
+	return (0)
+}
+
