38b055006881307023a64fd7fee3cdfe1c2c016f
LavoussierDev <Onilr31@gmail.com>
2023-11-25 02:40:07 -0400

    Backing up files

--- printf.c
+++ printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "main.h"
 
 /**
@@ -7,15 +8,63 @@
  * numbers, special characters, essentially it's a clone
  * to the printf function in the standard library
  *
- * @format:
+ * _putchar - outputs a character to the standard output
+ *
+ * @format: pointer to a string, contains format specifiers,
+ * allows us to print said character
  *
  */
 
-int _printf(const char *format, ...)
+void _putchar(char c) /* implicit delcaration error */
 {
-
-
-
-	return (0)
+	write(1, &c, 1);
 }
 
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	while (*format != '\0') /* Needs to be placed in the numericals file */
+	{
+		if (*format == '%' && *(format + 1) == 'd')
+		{
+			int value = va_arg(args, int);
+
+            /* Print the integer digit by digit */
+			int divisor = 1;
+			while (value / divisor > 9)
+			{
+				divisor *= 10;
+            }
+			while (divisor != 0)
+			{
+				_putchar('0' + value / divisor);
+				value %= divisor;
+				divisor /= 10;
+			}
+
+				format += 2; /* Move past '%d' */
+        }
+		else if (*format == '%' && *(format + 1) == 's')
+		{
+			char *str = va_arg(args, char *);
+
+            /* Print each character of the string */
+			while (*str != '\0')
+			{
+				_putchar(*str);
+				str++;
+            }
+
+			format += 2; /* Move past '%s' */
+        } else
+		{
+			_putchar(*format);
+			format++;
+		}
+	}
+
+	va_end(args);
+	return (0); /* return could potentially be changed */
+}
