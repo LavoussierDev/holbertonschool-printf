6260f5e3cc7c5db619fb6bc21266d509760bac84
kriverao98 <riveraocasiokoral29@gmail.com>
2023-11-25 20:56:53 -0400

    numerals.c

--- numerals.c
+++ numerals.c
@@ -3,70 +3,87 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...) {
-	 int num_print = 0;
-	 int len;
-	 char *str;
+/**
+ * _printf - Custom printf function
+ * @format: Format string containing conversion specifiers
+ * @...: Variable number of arguments corresponding to the specifiers
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
 
-	 va_list args;
 
-	 va_start(args, format);
+int _printf(const char *format, ...)
+{
+	int num_print = 0;
+	int len;
+	char *str;
 
-	 while (*format != '\0')
-	 {
+	va_list args;
+	va_start(args, format);
 
-		 if (*format == '%')
-		 {
-			 format++;
+	while (*format != '\0')
+	{
+		/* Move to char after % */
 
-			 if (*format == 'd' || *format == 'i')
-			 {
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == 'd' || *format == 'i')
+			{
+
+				/* get int argument, allocate memory */
 
 				num_print = va_arg(args, int);
 
-
 				str = malloc(12);
 
+				/* Check for memory allocation failure */
 
-				 if (str == NULL)
-				 {
-					 return -1;
-				 }
+				if (str == NULL)
+				{
+					return -1;
+				}
 
+				/*
+				 * Convert int to string, write string to stdout,
+				 * update the total of char printed
+				 * free string
+				 */
 
 				len = sprintf(str, "%d", num_print);
 
 				write(1, str, len);
+				num_print += len;
 
-				 num_print += len;
-
-				 free(str);
-
+				free(str);
 			}
 
-			else
-			{
-				 write(1, "%", 1);
+			else {
+				/**
+				 *  Write % char to stdout, write the char after it,
+				 *  and update the total char printed
+				 */
 
+				write(1, "%", 1);
 				write(1, format, 1);
-
 				num_print += 2;
-
 			}
-		 }
+		}
 
-		 else {
+		else {
 
-			 write(1, format, 1);
+			/* Write char to stdout and update the total of char printed */
 
-			 num_print++;
+			write(1, format, 1);
+			num_print++;
+		}
 
-		 }
+		/* Move to the next char in the string */
 
-		 format++;
+		format++;
+	}
 
-	 }
-
-	 va_end(args);
-	 return num_print;
+	va_end(args);
+	return num_print;
 }
